5b278b7c9d938e9d1bb2f71be3a001e18d14efc4
Kuba Jaroszewski <jakub.jaroszewski@gmail.com>
2026-01-11 21:35:37 +0100

    Set up posthog

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './i18n'
 import './index.css'
 import App from './App.tsx'
+import { PostHogProvider } from 'posthog-js/react'
+
+const options = {
+  api_host: 'https://wieprz.lessismore.studio/eu',
+  ui_host: 'https://eu.posthog.com',
+  defaults: '2025-11-30',
+} as const;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <PostHogProvider apiKey="phc_aaF1FEvqBY5V6BPwhoIrWEZFjg8L5KZjqgTJw8eKd51" options={options}>
+      <App />
+    </PostHogProvider>
   </StrictMode>,
 )
